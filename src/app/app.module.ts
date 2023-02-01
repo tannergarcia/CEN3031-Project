59740b788bdbf717de3dadae6344606547d57756
AsztiC <aszti.c3@gmail.com>
2023-01-31 23:38:22 -0500

    Basic visual features
    Created toolbar, buttons, textbox components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { ImagebtnsComponent } from './imagebtns/imagebtns.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { TextboxComponent } from './textbox/textbox.component';
+import {MatInputModule} from '@angular/material/input';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ImagebtnsComponent,
+    TextboxComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
