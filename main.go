b6975fa04a9a29278d62627381c578c66abbde42
tannergarcia <92037724+tannergarcia@users.noreply.github.com>
2023-02-05 13:22:58 -0500

    Provides a basic login using google login.  Proof of concept so that further functionality can be worked on. Uses environment variables for google API keys Will provide outside of github. Users are given a folder in which their images will be stored. Should go back later and also implement our own login and database.

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+
+	"log"
+
+	"github.com/gorilla/pat"
+	"github.com/gorilla/sessions"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+)
+
+func main() {
+
+	//key := "" // Replace with your SESSION_SECRET or similar
+
+	// key should be set in environment variable
+	// use "set GOOGLEKEY={key}" on windows
+	// may have to go into control pannel if that doesn't work
+	// I had to restart for it to apply
+	key := os.Getenv("GOOGLE_KEY")
+
+	// ClientID also set in environment variable
+	
+	clientID := os.Getenv("GOOGLE_CLIENT")
+
+
+
+	maxAge := 86400 * 30                         // 30 days
+	isProd := false                              // Set to true when serving over https
+
+	store := sessions.NewCookieStore([]byte(key))
+	store.MaxAge(maxAge)
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true // HttpOnly should always be enabled
+	store.Options.Secure = isProd
+
+	gothic.Store = store
+
+	goth.UseProviders(
+		google.New(clientID, key, "http://localhost:3000/auth/google/callback", "email", "profile"),
+	)
+
+	p := pat.New()
+	p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
+
+		user, err := gothic.CompleteUserAuth(res, req)
+		if err != nil {
+			fmt.Fprintln(res, err)
+			return
+		}
+		t, _ := template.ParseFiles("templates/success.html")
+
+		if _, err := os.Stat("./userImages/" + user.UserID); !os.IsNotExist(err) {
+			fmt.Println("Existing User, user file exists")
+		} else {
+			os.MkdirAll("./userImages/"+user.UserID, os.ModePerm)
+			fmt.Println("New User, Created user Images file")
+		}
+
+		t.Execute(res, user)
+	})
+
+	p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		gothic.BeginAuthHandler(res, req)
+	})
+
+	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		t, _ := template.ParseFiles("templates/index.html")
+		t.Execute(res, false)
+	})
+	log.Println("listening on localhost:3000")
+	log.Fatal(http.ListenAndServe(":3000", p))
+}
