e7e699e85d6a451bdb2f459247554d72d7ca4d90
tannergarcia <92037724+tannergarcia@users.noreply.github.com>
2023-02-05 19:08:18 -0500

    Changed from google authentication to our own user/pass implementation Uses cookies to retain user sessions Users are saved in a mysql database Each User has a unique ID, automatically incrementing from 1 each user has a session attached in the database Passwords are encrypted using bcrypt

--- main.go
+++ main.go
@@ -1,85 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
-	"os"
-
 	"log"
+	"github.com/tannergarcia/PhotoBomb/database"
+	"github.com/tannergarcia/PhotoBomb/auth"
 
-	"github.com/gorilla/pat"
-	"github.com/gorilla/sessions"
-	"github.com/markbates/goth"
-	"github.com/markbates/goth/gothic"
-	"github.com/markbates/goth/providers/google"
 )
 
 func main() {
 
-	//key := "" // Replace with your SESSION_SECRET or similar
-
-	// key should be set in environment variable
-	// use "set GOOGLEKEY={key}" on windows
-	// may have to go into control pannel if that doesn't work
-	// I had to restart for it to apply
-	key := os.Getenv("GOOGLE_KEY")
-
-	// ClientID also set in environment variable
-	
-	clientID := os.Getenv("GOOGLE_CLIENT")
+	database.Connect()
+	database.Migrate()
 
 
+	http.HandleFunc("/signin", auth.Signin)
+	http.HandleFunc("/signup", auth.Signup)
+	http.HandleFunc("/logout", auth.Logout)
 
-	maxAge := 86400 * 30                         // 30 days
-	isProd := false                              // Set to true when serving over https
-
-	store := sessions.NewCookieStore([]byte(key))
-	store.MaxAge(maxAge)
-	store.Options.Path = "/"
-	store.Options.HttpOnly = true // HttpOnly should always be enabled
-	store.Options.Secure = isProd
-
-	gothic.Store = store
-
-	goth.UseProviders(
-		google.New(clientID, key, "http://localhost:3000/auth/google/callback", "email", "profile"),
-	)
-
-	p := pat.New()
-	p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
-
-		user, err := gothic.CompleteUserAuth(res, req)
-		if err != nil {
-			fmt.Fprintln(res, err)
-			return
-		}
-		t, _ := template.ParseFiles("templates/success.html")
-
-		store.Save
-
-		t.Execute(res, user)
-	})
-
-	p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
-	  if gothUser, err := gothic.CompleteUserAuth(res, req); err == mil {
-		t, _ := template.ParseFiles("templates/success.html")
-		t.Execute(res, gothUser)
-	  } else {
-	    gothic.BeginAuthHandler(res, req)
-	  }
-	})
-
-	p.Get("/logout/{provider}", func(res http.ResponseWriter, req *http.Request)) {
-		gothic.logout(res,req)
-		res.Header().set("Location", "/")
-		res.WriteHeader(http.StatusTemporaryRedirect)
-	}
-
-	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
-		t, _ := template.ParseFiles("templates/index.html")
-		t.Execute(res, false)
-	})
 	log.Println("listening on localhost:3000")
-	log.Fatal(http.ListenAndServe(":3000", p))
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
