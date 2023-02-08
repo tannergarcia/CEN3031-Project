5fff0ad300dc973cb9a932e1319cd7f20fee0e8b
EricUF <92952501+EricUF@users.noreply.github.com>
2023-02-08 17:12:05 -0500

    Delete app directory

--- app/shared/directives/fullpage.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FullpageDirective } from './fullpage.directive';
-
-describe('FullpageDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FullpageDirective();
-    expect(directive).toBeTruthy();
-  });
-});
