5fb56cc8f6733bb12369c20dc8967ab6938b9c81
tannergarcia <92037724+tannergarcia@users.noreply.github.com>
2023-02-28 23:54:54 -0500

    fixed spelling

--- pkg/models/image.go
+++ pkg/models/image.go
@@ -4,5 +4,5 @@ type Image struct {
 	ID        uint   `json:"id"`
 	Token     string `json:"token"`     //Unique id per user
 	Timestamp string `json:"timestamp"` //Name of image file
-	Extention string `json:"extention"` //File extension
+	Extension string `json:"extention"` //File extension
 }
